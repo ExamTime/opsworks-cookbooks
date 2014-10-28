47e940c62ab680f700e6149619e4c625b410dedb
Pablo V Caderno <pablo.caderno@examtime.com>
2014-10-28 11:49:09 +0000

    Removed owner and group from files

--- nginx/recipes/configure.rb
+++ nginx/recipes/configure.rb
@@ -66,8 +66,8 @@ deny all;',
 end
 
 cookbook_file "/etc/nginx/common/proxy.conf" do
-  owner deploy[:user]
-  group deploy[:group]
+#  owner deploy[:user]
+#  group deploy[:group]
   mode 0644
   source 'proxy.conf'
   backup false
@@ -76,8 +76,8 @@ end
 
 
 cookbook_file "/etc/nginx/http-custom.conf" do
-  owner deploy[:user]
-  group deploy[:group]
+#  owner deploy[:user]
+#  group deploy[:group]
   mode 0644
   source "http-custom.conf"
   backup false
@@ -91,15 +91,15 @@ src_filepath = "/etc/nginx"
 
 
 cookbook_file "#{src_filepath}/#{src_filename1}" do
-  owner deploy[:user]
-  group deploy[:group]
+#  owner deploy[:user]
+#  group deploy[:group]
   mode 0644
   source "#{src_filename1}"
 end
 
 cookbook_file "#{src_filepath}/#{src_filename2}" do
-  owner deploy[:user]
-  group deploy[:group]
+#  owner deploy[:user]
+#  group deploy[:group]
   mode 0644
   source "#{src_filename2}"
 end
