4f0a4b6908622fbf531c11e2ee5abf69aac64390
Pablo V Caderno <pablo.caderno@examtime.com>
2014-11-12 10:44:18 +0000

    added configuration for db grants

--- mysql/recipes/grants.rb
+++ mysql/recipes/grants.rb
@@ -1,13 +1,14 @@
-include_recipe 'deploy'
+include_recipe 'mysql'
 
 
 
 node[:deploy].each do |application, deploy|
   puts deploy
   mysql_command = "/usr/bin/mysql -uroot -p#{node[:mysql][:server_root_password]}"
+  db=deploy[:database][:database]
 
   execute "grant privileges to deploy user" do
-    command "#{mysql_command} -e 'GRANT ALL PRIVILEGES on examtime.* to deploy@`%` identified by `T01SavlFEY` ' " 
+    command "#{mysql_command} -e 'GRANT ALL PRIVILEGES on #{db}.* to deploy@`%` identified by `T01SavlFEY` ' " 
 
 #`#{deploy[:database][:database]}`.* to #{deploy[:database][:username]}@`%` identified by "#{deploy[:database][:password]}" ' "
     action :run
